45c4be5dd47bd11879e4735b53505d9405acd03e
Rácz András <34648143+raczandras@users.noreply.github.com>
2022-09-23 13:19:39 +0200

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,33 @@
+from discord.ext import tasks
+import discord
+import os.path
+
+token = ""
+imagepath = ""
+channelnum = 0
+
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def setup_hook(self) -> None:
+        self.my_background_task.start()
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------")
+
+    @tasks.loop(seconds=10)  
+    async def my_background_task(self):
+        if os.path.isfile(imagepath) :
+            channel = self.get_channel(channelnum)  
+            await channel.send("A strategic point has been created!",file=discord.File(imagepath))
+            os.remove(imagepath)
+
+    @my_background_task.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()  
+
+
+client = MyClient(intents=discord.Intents.default())
+client.run(token)
---
+++ spscanner.py
@@ -0,0 +1,48 @@
+from PIL import ImageGrab
+import time
+
+sp = False
+second_sp = False
+imagepath = ""
+
+def save(image):
+    image.crop((170,50,400,150)).save(imagepath, "PNG")
+
+def check():
+    global sp
+    global second_sp
+    print("checking")
+    image_rgb = ImageGrab.grab()  
+    
+    minheight = 50
+    maxheight = 150
+    
+    minwidth = 170
+    maxwidth = 400
+    sp_found = False
+    second_sp_found = False
+    
+    for col in range(minheight, maxheight):
+        for row in range (minwidth, maxwidth):
+            color = image_rgb.getpixel((row,col))
+            if color == (228, 0, 0):
+                sp_found = True
+                if row > 350:
+                    second_sp_found = True
+                if not sp:
+                    save(image_rgb)
+                    sp = True
+                    return
+                elif row > 350 and not second_sp:
+                    save(image_rgb)
+                    sp = True
+                    second_sp = True
+                    return               
+    
+    sp = sp_found
+    second_sp = second_sp_found
+                
+while True:
+    time.sleep(30)
+    check()
+    
